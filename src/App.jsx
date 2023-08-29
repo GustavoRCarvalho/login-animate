4b03e2dd61e954772b9f0ef7be01c500c1acefea
gustavo-paipe <gustavorafaeldecarvalho@gmail.com>
2023-08-29 16:13:20 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Login } from "./components/screens/Login"
+
+export const App = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  )
+}
---
+++ src/components/common/ContentLimit.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+
+export const ContentContainer = styled.section`
+  position: relative;
+
+  max-width: 1440px;
+  width: 95vw;
+`
